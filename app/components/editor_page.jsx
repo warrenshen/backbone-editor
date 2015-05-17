82c3c4c50e97cd5ef612d5589096f17bc40f220e
warren <warrenzshen@gmail.com>
2015-05-17 15:10:12 -0700

    Create editor page component

---
+++ app/components/editor_page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ListeningComponent from "app/templates/listening_component";
+
+
+class EditorPage extends ListeningComponent {
+
+  render() {
+    return (
+      <div className={"general-page"}>
+        Welcome to the editor page.
+      </div>
+    );
+  }
+}
+
+
+module.exports = EditorPage;
