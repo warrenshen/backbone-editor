a721d9c85da689df1b838dd8d262c2444fd17957
warren <warrenzshen@gmail.com>
2015-05-17 14:05:52 -0700

    Initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    __dirname + "/app/app.js",
+  ],
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+  module: {
+    loaders: [
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "react-hot-loader!babel-loader" },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.scss$/, exclude: /node_modules/, loader: "style!css!sass" },
+    ]
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ["", ".js", ".jsx"],
+  },
+}
