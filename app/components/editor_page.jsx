a5d0d582a634d305995e2d0c489528d9ad35cf56
warren <warrenzshen@gmail.com>
2015-05-18 23:30:51 -0700

    Set up editor page to listen to editor store

--- app/components/editor_page.jsx
+++ app/components/editor_page.jsx
@@ -3,14 +3,27 @@ import ListeningComponent from "app/templates/listening_component";
 
 import StoryComponent from "app/components/story";
 
+import EditorStore from "app/stores/editor_store";
+
 
 class EditorPage extends ListeningComponent {
 
+  stores() {
+    return [EditorStore];
+  }
+
+  getStoreState() {
+    return {
+      story: EditorStore.getCurrent(),
+    }
+  }
+
   render() {
+    console.log(this.state.story);
     return (
       <div style={this.styles.container}>
         Welcome to the editor page.
-        <StoryComponent />
+        <StoryComponent story={this.state.story} />
       </div>
     );
   }
