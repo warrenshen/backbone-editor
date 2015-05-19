5cea4e6387bb75052088ce6926a453ac58a7849e
warren <warrenzshen@gmail.com>
2015-05-18 20:14:50 -0700

    Create section component

--- app/components/editor_page.jsx
+++ app/components/editor_page.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import ListeningComponent from "app/templates/listening_component";
 
+import Section from "app/components/section";
+
 import Story from "app/models/story";
 
 
 class EditorPage extends ListeningComponent {
 
+  renderSection(section) {
+    <Section key={section.get("cid")} section={section} />
+  }
+
+  renderSections() {
+    var sections = this.props.story.get("sections");
+    return sections.map(this.renderSection(), this);
+  }
+
   render() {
     return (
       <div className={"general-page"}>
         Welcome to the editor page.
+        {this.renderSections()}
       </div>
     );
   }
---
+++ app/components/section.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Component from "app/templates/component";
+
+
+class Section extends Component {
+
+  render() {
+    return (
+      <span>test section</span>
+    );
+  }
+}
+
+
+module.exports = Section;
