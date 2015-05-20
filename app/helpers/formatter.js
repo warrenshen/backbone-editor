6c9e2edc0df7d667b24f175a9d0c7957b08b5986
warren <warrenzshen@gmail.com>
2015-05-19 18:55:49 -0700

    Create formatter helper module

---
+++ app/helpers/formatter.js
@@ -0,0 +1,52 @@
+class Formatter {
+
+  format(block) {
+    var elements = block.get("elements");
+    var characters = block.get("content").split("");
+    var openers = {};
+    var closers = {};
+    parse(elements, openers, closers);
+    return merge(characters, openers, closers);
+  }
+
+  parse(elements, openers, closers) {
+    for (element : elements) {
+      var start = element.get("start");
+      var end = element.get("end");
+      var opener = "";
+      var closer = "";
+      switch (element.get("type")) {
+        case "bold":
+          opener = "strong";
+          closer = "strong";
+          break;
+      }
+      if (openers[start]) {
+        openers[start].push("<" + opener + ">");
+      } else {
+        openers[start] = ["<" + opener + ">"]
+      }
+      if (closers[end]) {
+        closers[end].push("</" + closer +">");
+      } else {
+        closers[end] = ["</" + closer + ">"];
+      }
+    }
+  }
+
+  merge(characters, openers, closers) {
+    var content = "";
+    for (i = 0; i < characters.length; i += 1) {
+      if (openers[i]) {
+        content += openers[i];
+      } else if (closers[i]) {
+        content += closers[i];
+      }
+      content += characters[i];
+    }
+    return content;
+  }
+}
+
+
+module.exports = new Formatter();
