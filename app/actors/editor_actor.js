6e627d6152400a2c9dba92de45fc91342501b767
warren <warrenzshen@gmail.com>
2015-05-20 18:03:44 -0700

    Create template actor class and editor actor child

---
+++ app/actors/editor_actor.js
@@ -0,0 +1,16 @@
+import Actions from "app/templates/actor";
+import ActionConstants from "app/constants/action_constants";
+
+
+class EditorActor extends Actor {
+
+  create(attributes) {
+    this.act({
+      type: ActionConstants.editor.create,
+      attributes: attributes,
+    });
+  }
+}
+
+
+module.exports = new EditorActor();
---
+++ app/constants/action_constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  editor: {
+    create: "create",
+  },
+}
