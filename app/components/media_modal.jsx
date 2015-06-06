174296d5a26c9763fa05456f0cdcee0876a6e79b
warren <warrenzshen@gmail.com>
2015-06-05 20:27:45 -0700

    Create media modal options

--- app/components/media_modal.jsx
+++ app/components/media_modal.jsx
@@ -5,17 +5,52 @@ import Component from "app/templates/component";
 
 class MediaModal extends Component {
 
+  getDefaultState() {
+    return { shouldShowOptions: false };
+  }
+
+  handleClick(event) {
+    console.log("Prompt clicked!");
+    this.setState({ shouldShowOptions: !this.state.shouldShowOptions });
+  }
+
+  componentDidMount() {
+    var prompt = React.findDOMNode(this.refs.prompt);
+    prompt.addEventListener("click", this.handleClick.bind(this));
+  }
+
+  componentWillUnmount() {
+    var prompt = React.findDOMNode(this.refs.prompt);
+    prompt.removeEventListener("click", this.handleClick);
+  }
+
   render() {
     var modalClass = ClassNames(
       { "media-modal": true },
       { "general-hidden": false }
     );
+    var optionClass = ClassNames(
+      { "media-modal-option": true },
+      { "media-modal-option-hidden": !this.state.shouldShowOptions }
+    );
     return (
       <div className={modalClass} ref="modal">
         <span className={"vertical-anchor"}></span>
-        <span className={"media-modal-prompt"}>
+        <span className={"media-modal-prompt"} ref="prompt">
           <span className={"vertical-anchor"}></span>
           <i className={"fa fa-plus"}></i>
+        </span>
+        <span className={optionClass} ref={"image"}>
+          <span className={"vertical-anchor"}></span>
+          <i className={"fa fa-image"}></i>
+        </span>
+        <span className={optionClass} ref={"divider"}>
+          <span className={"vertical-anchor"}></span>
+          <i className={"fa fa-minus"}></i>
+        </span>
+        <span className={optionClass} ref={"divider"}>
+          <span className={"vertical-anchor"}></span>
+          <i className={"fa fa-code"}></i>
         </span>
       </div>
     );
