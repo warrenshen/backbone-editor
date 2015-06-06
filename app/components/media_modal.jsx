3fe4e770cb9935d144996604cf08bac3ba35f175
warren <warrenzshen@gmail.com>
2015-06-06 12:59:56 -0700

    Connect media modal event handlers

--- app/components/media_modal.jsx
+++ app/components/media_modal.jsx
@@ -13,19 +13,62 @@ class MediaModal extends Component {
     this.setState({ shouldShowOptions: false });
   }
 
+  handleClickCode(event) {
+
+  }
+
+  handleClickDivider(event) {
+
+  }
+
+  handleClickImage(event) {
+
+  }
+
+  handleClickPrompt(event) {
+    React.findDOMNode(this.refs.invisible).focus();
+    this.setState({ shouldShowOptions: !this.state.shouldShowOptions });
+  }
+
   handleMouseDown(event) {
     event.preventDefault();
     event.stopPropagation();
   }
 
-  handlePromptClick(event) {
-    React.findDOMNode(this.refs.invisible).focus();
-    this.setState({ shouldShowOptions: !this.state.shouldShowOptions });
+  componentDidMount() {
+    var code = React.findDOMNode(this.refs.code);
+    code.addEventListener("click", this.handleClickCode.bind(this));
+
+    var divider = React.findDOMNode(this.refs.divider);
+    divider.addEventListener("click", this.handleClickDivider.bind(this));
+
+    var image = React.findDOMNode(this.refs.image);
+    image.addEventListener("click", this.handleClickImage.bind(this));
+
+    var invisible = React.findDOMNode(this.refs.invisible);
+    invisible.addEventListener("blur", this.handleBlur.bind(this));
+
+    var prompt = React.findDOMNode(this.refs.prompt);
+    prompt.addEventListener("click", this.handleClickPrompt.bind(this));
+    prompt.addEventListener("mousedown", this.handleMouseDown.bind(this));
   }
 
-  componentDidMount() {
+  componentWillUnmount() {
+    var code = React.findDOMNode(this.refs.code);
+    code.removeEventListener("click", this.handleClickCode);
+
+    var divider = React.findDOMNode(this.refs.divider);
+    divider.removeEventListener("click", this.handleClickDivider);
+
+    var image = React.findDOMNode(this.refs.image);
+    image.removeEventListener("click", this.handleClickImage);
+
+    var invisible = React.findDOMNode(this.refs.invisible);
+    invisible.removeEventListener("blur", this.handleBlur);
+
     var prompt = React.findDOMNode(this.refs.prompt);
-    prompt.addEventListener("mousedown", this.handleMouseDown.bind(this));
+    prompt.removeEventListener("click", this.handleClickPrompt);
+    prompt.removeEventListener("mousedown", this.handleMouseDown);
   }
 
   render() {
@@ -43,25 +86,21 @@ class MediaModal extends Component {
         <p
           className={"general-invisible"}
           contentEditable={"true"}
-          onBlur={this.handleBlur.bind(this)}
           ref={"invisible"}>
         </p>
-        <span
-          className={"media-modal-prompt"}
-          onClick={this.handlePromptClick.bind(this)}
-          ref={"prompt"}>
+        <span className={"media-modal-prompt"} ref={"prompt"}>
           <span className={"vertical-anchor"}></span>
           <i className={"fa fa-plus"}></i>
         </span>
-        <span className={optionClass}>
+        <span className={optionClass} ref={"image"}>
           <span className={"vertical-anchor"}></span>
           <i className={"fa fa-image"}></i>
         </span>
-        <span className={optionClass}>
+        <span className={optionClass} ref={"divider"}>
           <span className={"vertical-anchor"}></span>
           <i className={"fa fa-minus"}></i>
         </span>
-        <span className={optionClass}>
+        <span className={optionClass} ref={"code"}>
           <span className={"vertical-anchor"}></span>
           <i className={"fa fa-code"}></i>
         </span>
