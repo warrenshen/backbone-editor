c893d65daca75e9447d5e97c5d2a90a97f6e4ce8
warren <warrenzshen@gmail.com>
2015-06-06 22:08:03 -0700

    Support deletion of image blocks

--- app/components/block_image.jsx
+++ app/components/block_image.jsx
@@ -2,37 +2,102 @@ import ClassNames from "classnames";
 import React from "react";
 import BlockComponent from "app/templates/block_component";
 
+import EditorActor from "app/actors/editor_actor";
+
+import Point from "app/helpers/point";
+
+import KeyConstants from "app/constants/key_constants";
+
 
 class BlockImage extends BlockComponent {
 
   getDefaultState() {
-    return { shouldShowPlaceholder: true };
+    return {
+      shouldShowBorder: false,
+      shouldShowPlaceholder: true,
+    };
   }
 
-  handleBlur(event) {
+  generatePoint() {
+    return new Point(
+      this.props.sectionIndex,
+      this.props.block.get("index"),
+      0
+    );
+  }
+
+  handleBlurCaption(event) {
     if (!this.props.block.get("content")) {
       this.setState({ shouldShowPlaceholder: true });
     }
   }
 
-  handleFocus(event) {
+  handleBlurImage(event) {
+    if (this.state.shouldShowBorder) {
+      this.setState({ shouldShowBorder: false });
+    }
+  }
+
+  handleClick(event) {
+    if (!this.state.shouldShowBorder) {
+      React.findDOMNode(this.refs.invisible).focus();
+    }
+  }
+
+  handleFocusCaption(event) {
     if (this.state.shouldShowPlaceholder) {
       this.setState({ shouldShowPlaceholder: false });
     }
   }
 
+  handleFocusImage(event) {
+    this.setState({ shouldShowBorder: true });
+  }
+
+  handleKeyDown(event) {
+    console.log("ho");
+    event.preventDefault();
+    if (event.which === KeyConstants.backspace) {
+      var point = this.generatePoint();
+      EditorActor.removeBlock(point);
+      this.props.updateStory();
+    }
+  }
+
+  handleMouseDown(event) {
+    event.preventDefault();
+  }
+
   componentDidMount() {
     super.componentDidMount();
     var content = React.findDOMNode(this.refs.content);
-    content.addEventListener("blur", this.handleBlur.bind(this));
-    content.addEventListener("focus", this.handleFocus.bind(this));
+    content.addEventListener("blur", this.handleBlurCaption.bind(this));
+    content.addEventListener("focus", this.handleFocusCaption.bind(this));
+
+    var image = React.findDOMNode(this.refs.image);
+    image.addEventListener("click", this.handleClick.bind(this));
+    image.addEventListener("mousedown", this.handleMouseDown.bind(this));
+
+    var invisible = React.findDOMNode(this.refs.invisible);
+    invisible.addEventListener("blur", this.handleBlurImage.bind(this));
+    invisible.addEventListener("focus", this.handleFocusImage.bind(this));
+    invisible.addEventListener("keydown", this.handleKeyDown.bind(this));
   }
 
   componentWillUnmount() {
     super.componentWillUnmount();
     var content = React.findDOMNode(this.refs.content);
-    content.removeEventListener("blur", this.handleBlur);
-    content.removeEventListener("focus", this.handleFocus);
+    content.removeEventListener("blur", this.handleBlurCaption);
+    content.removeEventListener("focus", this.handleFocusCaption);
+
+    var image = React.findDOMNode(this.refs.image);
+    image.removeEventListener("click", this.handleClick);
+    image.removeEventListener("mousedown", this.handleMouseDown);
+
+    var invisible = React.findDOMNode(this.refs.invisible);
+    invisible.removeEventListener("blur", this.handleBlurImage);
+    invisible.removeEventListener("focus", this.handleFocusImage);
+    invisible.removeEventListener("keydown", this.handleKeyDown);
   }
 
   render() {
@@ -41,12 +106,19 @@ class BlockImage extends BlockComponent {
       { "block-image-caption": true },
       { "general-placeholder": this.state.shouldShowPlaceholder }
     );
+    var imageClass = ClassNames(
+      { "block-image": true },
+      { "block-image-bordered": this.state.shouldShowBorder }
+    );
     return (
       <div
         className={"block-container"}
         data-index={block.get("index")}>
         <div className={"block-image-container"}>
-          <img className={"block-image"} src={block.get("source")} />
+          <img
+            className={imageClass}
+            ref={"image"}
+            src={block.get("source")} />
           <p
             className={"general-invisible"}
             contentEditable={"true"}
