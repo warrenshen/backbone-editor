e157087552e63ec7adef0f730ce866ec6756dba0
warren <warrenzshen@gmail.com>
2015-06-30 21:59:39 -0700

    Change block image to react to hover

--- app/components/edit/block_image.jsx
+++ app/components/edit/block_image.jsx
@@ -27,109 +27,76 @@ class BlockImage extends Component {
   // State
   // --------------------------------------------------
   getDefaultState() {
-    return { shouldShowBorder: false };
+    return { shouldShowOptions: false };
   }
 
   // --------------------------------------------------
   // Handlers
   // --------------------------------------------------
-  handleBlur(event) {
-    var node = React.findDOMNode(this.refs.input);
-    if (node && this.state.shouldShowBorder) {
-      this.setState({ shouldShowBorder: false });
-    }
-  }
-
   handleClick(event) {
-    if (this.props.block.get("source") === "placeholder") {
-      console.log("Should prompt image upload!");
-    } else if (!this.state.shouldShowBorder) {
-      React.findDOMNode(this.refs.input).focus();
+    console.log("click");
+  }
+
+  handleMouseEnter(event) {
+    if (!this.state.shouldShowOptions) {
+      this.setState({ shouldShowOptions: true });
     }
   }
 
-  handleFocus(event) {
-    this.setState({ shouldShowBorder: true });
-  }
-
-  handleKeyDown(event) {
-    event.preventDefault();
-    if (event.which === KeyConstants.backspace) {
-      var block = this.props.block;
-      var point = new Point(
-        block.get("section_index"),
-        block.get("index"),
-        0
-      );
-      EditorActor.removeBlock(point);
-      this.props.updateStoryEditable();
+  handleMouseLeave(event) {
+    if (this.state.shouldShowOptions) {
+      this.setState({ shouldShowOptions: false });
     }
-  }
-
-  handleMouseDown(event) {
-    event.preventDefault();
-  }
-
-  handleMouseUp(event) {
-    event.stopPropagation();
   }
 
   // --------------------------------------------------
   // Lifecycle
   // --------------------------------------------------
   componentDidMount() {
-    var node = React.findDOMNode(this.refs.image);
+    var node = React.findDOMNode(this.refs.container);
     node.addEventListener("click", this.handleClick.bind(this));
-    node.addEventListener("mousedown", this.handleMouseDown.bind(this));
-    node.addEventListener("mouseup", this.handleMouseUp.bind(this));
-    node = React.findDOMNode(this.refs.input);
-    node.addEventListener("blur", this.handleBlur.bind(this));
-    node.addEventListener("focus", this.handleFocus.bind(this));
-    node.addEventListener("keydown", this.handleKeyDown.bind(this));
+    node.addEventListener("mouseenter", this.handleMouseEnter.bind(this));
+    node.addEventListener("mouseleave", this.handleMouseLeave.bind(this));
   }
 
   componentWillUnmount() {
-    var node = React.findDOMNode(this.refs.image);
+    var node = React.findDOMNode(this.refs.container);
     node.removeEventListener("click", this.handleClick);
-    node.removeEventListener("mousedown", this.handleMouseDown);
-    node.removeEventListener("mouseup", this.handleMouseUp);
-    node = React.findDOMNode(this.refs.input);
-    node.removeEventListener("blur", this.handleBlur);
-    node.removeEventListener("focus", this.handleFocus);
-    node.removeEventListener("keydown", this.handleKeyDown);
+    node.removeEventListener("mouseenter", this.handleMouseEnter);
+    node.removeEventListener("mouseleave", this.handleMouseLeave);
   }
 
   // --------------------------------------------------
   // Render
   // --------------------------------------------------
-  renderImage() {
-    var block = this.props.block;
-    var imageClass = ClassNames(
-      { "block-image": true },
-      { "block-image-bordered": this.state.shouldShowBorder },
-      { "block-image-placeholder": block.get("source") === "placeholder" }
-    );
-    return (
-      <div className={"block-image-container"}>
-        <img
-          className={imageClass}
-          ref={"image"}
-          src={block.get("source")} />
-        <input
-          className={"general-invisible"}
-          ref={"input"}>
-        </input>
-      </div>
-    );
+  renderOptions() {
+    if (this.state.shouldShowOptions) {
+      return (
+        <div className={"block-image-overlay"}>
+          <span className={"vertical-anchor"}></span>
+        </div>
+      );
+    }
   }
 
   render() {
+    var block = this.props.block;
+    var imageClass = ClassNames(
+      { "block-image": true },
+      { "block-image-placeholder": block.get("source") === "placeholder" }
+    );
     return (
       <div
         className={"block-container"}
         contentEditable={"false"}
-        data-index={this.props.block.get("index")}>
-        {this.renderImage()}
+        data-index={block.get("index")}
+        ref={"container"}>
+        <div className={"block-image-container"}>
+          <img
+            className={imageClass}
+            src={block.get("source")} />
+          {this.renderOptions()}
+        </div>
         <BlockCaption {...this.props} />
       </div>
     );
